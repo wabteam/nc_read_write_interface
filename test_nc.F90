42725739eec25cbe3164d36c80dfe8e0fcb34042
huangwenyu <huangwenyu@mail.tsinghua.edu.cn>
2013-05-05 00:50:57 +0800

    format the test_nc.F90

--- test_nc.F90
+++ test_nc.F90
@@ -19,16 +19,17 @@ program test_nc
     integer, parameter :: num_lev = 30 
     integer, parameter :: num_time = 1
 
-    integer :: time(num_time)
+    integer :: time (num_time)
     real (8) :: z0 (num_lon, num_lat, num_time) 
     real (8) :: us (num_lon, num_lat, num_lev, num_time) 
 
     call nc_read_write_interface_read_var (time, file_nc, "time", [1], [num_time], [num_time], 1)
-    call nc_read_write_interface_read_var (z0,file_nc,"z0",[1,1,1],[num_lon,num_lat,num_time],[num_lon,num_lat,num_time],3)
-    call nc_read_write_interface_read_var (us,file_nc,"us",[1,1,1,1],[num_lon,num_lat,num_lev,num_time],[num_lon,num_lat,num_lev,num_time],4)
+    call nc_read_write_interface_read_var (z0, file_nc, "z0", [1, 1, 1], [num_lon, num_lat, num_time], [num_lon, num_lat, num_time], 3)
+    call nc_read_write_interface_read_var (us, file_nc, "us", [1, 1, 1, 1], [num_lon, num_lat, num_lev, num_time], [num_lon, num_lat, num_lev, num_time],4)
+
     print *, time
-    print *, minval(z0), maxval(z0, mask = z0 < 100.0d0)
-    print *, minval(us), maxval(us, mask = us < 100.0d0)
+    print *, minval (z0), maxval (z0, mask = z0 < 100.0d0)
+    print *, minval (us), maxval (us, mask = us < 100.0d0)
     call nc_read_write_interface_delete_file ("hwy.nc")
     call nc_read_write_interface_create_file ("hwy.nc")
 
